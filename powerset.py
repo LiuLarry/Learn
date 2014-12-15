8fe4d3103fbbd9e0e5afba3de5a05148f7badfca
Li Min Liu <liminliu@cn.ibm.com>
2014-12-15 17:08:11 +0800

    powerset

---
+++ powerset.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def powerset(p):
+    aa =[[]]
+    for i in p:
+        l = len(aa)
+        for j in aa[:l]:
+            k = [i]
+            k = j + k
+            aa.append(k)
+
+    print aa
+
+powerset([1,2])
+
+powerset([1,2,3,4,5,6,7])
